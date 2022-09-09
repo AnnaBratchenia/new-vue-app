686e8880346cd9f323d82155f3bfab5d2a048a51
AnnaBratchenia <grigorevichanna@gmail.com>
2022-09-09 11:47:38 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <friend-contact></friend-contact>
+    <friend-contact></friend-contact>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>{{ friends.name }}</li>
+  <button v-on:click="toggleDetails">Show details</button>
+  <ul v-if="detailsIsvisible">
+    <li><strong>Phone: </strong>{{ friends.phone }}</li>
+    <li><strong>Email: </strong>{{ friends.email }}</li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      detailsIsvisible: false,
+      friends: {
+        id: "john",
+        name: "John Muller",
+        phone: "0957 340 509",
+        email: "johnmuller@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsIsvisible = !this.detailsIsvisible;
+    },
+  },
+};
+</script>
